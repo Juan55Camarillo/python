34581e25d327c590eaccbd499198bae12cfe2deb
unknown <caraga573@gmail.com>
2019-10-14 19:47:53 -0500

    Dibujo

---
+++ dibujo.py
@@ -0,0 +1,19 @@
+from turtle import *
+t = Pen()
+
+def mystart(size, filled):
+    if filled == True:
+        t.begin_fill()
+    for x in range(1, 19):
+        t.forward(size)
+        if x % 2 == 0:
+            t.left(175)
+        else:
+            t.left(225)
+    if filled == True:
+        t.end_fill()
+t.color(0.9, 0.75, 0)
+mystart(100,True)
+
+
+
